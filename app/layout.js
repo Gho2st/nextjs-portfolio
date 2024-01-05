97c7f3dde86a084439b589754ee2b8731779ca0c
Dominik Jojczyk <dominikjojczyk@air-dominik.home>
2024-01-05 18:45:14 +0100

    6115

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "@next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/Header";
+import { Analytics } from '@vercel/analytics/react'
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600"],
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
       >
         <Header />
         {children}
+        <Analytics/>
       </body>
     </html>
   );
