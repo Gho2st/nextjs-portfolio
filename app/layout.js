1102e48d2f05ca67a576710e9a3c5adb4d5b90af
Dominik Jojczyk <dominikjojczyk@air-dominik.home>
2024-01-05 16:35:40 +0100

    6v111215

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <body
         className={poppins.className}
       >
