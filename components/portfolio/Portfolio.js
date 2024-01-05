9e6a1d8f0d7a51a4a96a576e0bfaa74b2de82d9d
Dominik Jojczyk <dominikjojczyk@air-dominik.home>
2024-01-05 14:42:20 +0100

    6v115

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -17,12 +17,12 @@ export default function Portfolio() {
         </div>
       </div>
       <div className={classes.itemContainer}>
-        <PortfolioItem source="/optyk.png" title="Optyk - Andrzej Świerczek 1980" text="Strona internetowa wykonana w całości z uzyciem Bootstrapa." />
-        <PortfolioItem source="/beata.png" title="Beata Łakota - marka" text="Strona internetowa z wykorzystaniem TypeJS." />
-        <PortfolioItem source="/ozonowanie.png" title="EOS - ozonowanie | Kraków" text="Strona Główna z formularzem kontaktowym PHPMAILER." />
-        <PortfolioItem source="/ptaki.png" title="NSHKiPE - Nowy Sącz" text="Strona wraz z panelem admina napisanym w PHP + mySQL." />
-        <PortfolioItem source="/ericar.png" title="Ericar - dealer samochodowy." text="Projekt w którym wykorzystany był php, baza danych." />
-        <PortfolioItem source="/dominik.png" title="Dominik Jojczyk - portfolio" text="Mój projekt portfolio napisany całkowicie od zera w HTMl, CSS, JS." />
+        <PortfolioItem source="/optyk.png" href="https://optyk-andrzej.pl/" title="Optyk - Andrzej Świerczek 1980" text="Strona internetowa wykonana w całości z uzyciem Bootstrapa." />
+        <PortfolioItem source="/beata.png" href="https://beatalakota.pl/" title="Beata Łakota - marka" text="Strona internetowa z wykorzystaniem TypeJS." />
+        <PortfolioItem source="/ozonowanie.png" href="https://eos-ozonowanie.pl/" title="EOS - ozonowanie | Kraków" text="Strona Główna z formularzem kontaktowym PHPMAILER." />
+        <PortfolioItem source="/ptaki.png" href="https://nshkipe.com.pl/" title="NSHKiPE - Nowy Sącz" text="Strona wraz z panelem admina napisanym w PHP + mySQL." />
+        <PortfolioItem source="/ericar.png" href="https://github.com/Gho2st" title="Ericar - Github." text="Projekt w którym wykorzystany był php, baza danych. Znajduje sie na github." />
+        <PortfolioItem source="/dominik.png" href="https://dominikjojczyk.pl/" title="Dominik Jojczyk - portfolio" text="Mój projekt portfolio napisany całkowicie od zera w HTMl, CSS, JS." />
       </div>
     </>
   );
--- components/portfolio/PortfolioItem.js
+++ components/portfolio/PortfolioItem.js
@@ -1,25 +1,28 @@
+'use client'
 import classes from "./PortfolioItem.module.css";
 import Image from "next/image";
 
 export default function PortfolioItem(props) {
   return (
     <>
-      <div className={classes.item}>
-        <div className={classes.img}>
-          <Image
-            layout="responsive"
-            width={410}
-            height={250}
-            className={classes.image}
-            src={props.source}
-            alt={props.title}
-          />
+      
+        <div className={classes.item} onClick={() => window.location.href = props.href}>
+          <div className={classes.img}>
+            <Image
+              layout="responsive"
+              width={410}
+              height={250}
+              className={classes.image}
+              src={props.source}
+              alt={props.title}
+            />
+          </div>
+          <div className={classes.textContainer}>
+            <h5 className={classes.header}>{props.title}</h5>
+            <p className={classes.text}>{props.text}</p>
+          </div>
         </div>
-        <div className={classes.textContainer}>
-          <h5 className={classes.header}>{props.title}</h5>
-          <p className={classes.text}>{props.text}</p>
-        </div>
-      </div>
+      
     </>
   );
 }
