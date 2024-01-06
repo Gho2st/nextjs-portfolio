9b231f13d7e248ea43876074181237a76c3ea926
Dominik Jojczyk <dominikjojczyk@Air-Dominik.home>
2024-01-06 19:18:49 +0100

    new phot32

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -18,7 +18,7 @@ export default function Portfolio() {
       </div>
       <div className={classes.itemContainer}>
         <PortfolioItem source="/optyk.png" href="https://optyk-andrzej.pl/" title="Optyk - Andrzej Świerczek 1980" text="Strona internetowa wykonana w całości z uzyciem Bootstrapa." />
-        <PortfolioItem source="/beata.png" href="https://beatalakota.pl/" title="Beata Łakota - marka" text="Strona internetowa z wykorzystaniem TypeJS." />
+        <PortfolioItem source="/beata3.png" href="https://beatalakota.pl/" title="Beata Łakota - marka" text="Strona internetowa z wykorzystaniem TypeJS." />
         <PortfolioItem source="/ozonowanie.png" href="https://eos-ozonowanie.pl/" title="EOS - ozonowanie | Kraków" text="Strona Główna z formularzem kontaktowym PHPMAILER." />
         <PortfolioItem source="/ptaki.png" href="https://nshkipe.com.pl/" title="NSHKiPE - Nowy Sącz" text="Strona wraz z panelem admina napisanym w PHP + mySQL." />
         <PortfolioItem source="/ericar.png" href="https://github.com/Gho2st" title="Ericar - Github." text="Projekt w którym wykorzystany był php, baza danych. Znajduje sie na github." />
