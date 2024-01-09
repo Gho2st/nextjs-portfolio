8085476c153a11405c2091bdb9c4d31a5a35aa2e
Dominik Jojczyk <dominikjojczyk@air-dominik.home>
2024-01-09 21:21:08 +0100

    message to user after submitted f23m

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Poppins } from "@next/font/google";
+import { Poppins } from "@next/font-google";
 import "./globals.css";
 import Header from "@/components/ui/Header";
 import { Analytics } from "@vercel/analytics/react";
