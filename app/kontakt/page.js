da69436c5d8e33c844e691a7a91a963b3ddd4b0e
Dominik <dominik.jojczyk@gmail.com>
2024-01-23 21:08:58 +0100

    small issues updated

--- app/kontakt/page.js
+++ app/kontakt/page.js
@@ -17,23 +17,27 @@ export default function Kontakt() {
           <ContactItem
             icon={<FaLocationDot />}
             header="Location:"
-            info="Stacjonarnie - Kraków | Nowy Sącz"
+            info="Kraków | Nowy Sącz"
           />
           <ContactItem
             icon={<FaPhoneAlt />}
             header="Phone number:"
             info="+48 576 985 894"
           />
-          <ContactItem icon={<MdOutlineMailOutline />} header="Email:"
-            info="dominik.jojczyk@gmail.com" />
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d327953.3035028416!2d19.67523849478855!3d50.046298870558296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471644c0354e18d1%3A0xb46bb6b576478abf!2zS3Jha8Ozdw!5e0!3m2!1spl!2spl!4v1704144563818!5m2!1spl!2spl" 
-        
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"></iframe>
+          <ContactItem
+            icon={<MdOutlineMailOutline />}
+            header="Email:"
+            info="dominik.jojczyk@gmail.com"
+          />
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d327953.3035028416!2d19.67523849478855!3d50.046298870558296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471644c0354e18d1%3A0xb46bb6b576478abf!2zS3Jha8Ozdw!5e0!3m2!1spl!2spl!4v1704144563818!5m2!1spl!2spl"
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
-      <Socials/>
-      <Footer/>
+      <Socials />
+      <Footer />
     </>
   );
 }
--- components/homepage/Price.js
+++ components/homepage/Price.js
@@ -6,21 +6,20 @@ export default function Price() {
       <div className={classes.container}>
         <PriceItem
           header="Jaki jest koszt strony internetowej?"
-          text="Koszt projektu polegającego na stworzeniu strony internetowej na zamówienie zaczyna się od 600zł. Jednak zależy od wielu czynników, m.in.:
-
-— rodzaj i funkcjonalność,
-
-— złożoność projektu UX/UI,
-
-Z tego względu preferujemy wycenę na zamówienie indywidualnie po omówieniu całościowego zakresu projektu. Jeśli chcesz dowiedzieć się więcej na temat szacunków czeka na Ciebie formularz kontaktowy!"
+          text="Koszt projektu polegającego na stworzeniu strony internetowej na zamówienie zaczyna się od 600zł.
+          Preferuje wycenę na zamówienie indywidualnie po omówieniu całościowego zakresu projektu. Jeśli chcesz dowiedzieć się więcej na temat szacunków czeka na Ciebie formularz kontaktowy!"
         />
         <PriceItem
           header="Jaki jest czas realizacji projektu?"
-          text="Zazwyczaj czas od planowania do zrealizowania projektu w DJJ Software wynosi do tygodnia czasu. "
+          text="Zazwyczaj czas od planowania do zrealizowania projektu wynosi do tygodnia czasu. "
         />
         <PriceItem
           header="Ile kosztuje utrzymanie witryny internetowej?"
-          text="ceny hostingu (ok. 120-150zł rocznie), domeny (50zł rocznie). Cena zarejestrowania domeny na pierwszy rok jest -50%. "
+          text="Ceny hostingu + certyfikat SSL to ok. 120-150zł rocznie, domeny 50zł rocznie."
+        />
+        <PriceItem
+          header="Ile trzeba czekać na odpowiedź?"
+          text="W większości przypadków odpowiadam od razu, maksymalnie do paru godzin."
         />
       </div>
     </>
