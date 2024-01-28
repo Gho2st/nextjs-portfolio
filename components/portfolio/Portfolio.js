babf4053e5c8dde2316c58eab66f293acc96d9cb
Dominik <dominik.jojczyk@gmail.com>
2024-01-28 22:35:36 +0100

    responsive update

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -18,10 +18,10 @@ export default function Portfolio() {
       </div>
       <div className={classes.itemContainer}>
         <PortfolioItem source="/optyk.png" href="https://optyk-andrzej.pl/" title="Optyk - Andrzej Świerczek 1980" text="Strona internetowa wykonana w całości z uzyciem Bootstrapa." />
-        <PortfolioItem source="/beata3.png" href="https://beatalakota.pl/" title="Beata Łakota - trenerka" text="Strona internetowa z wykorzystaniem TypeJS." />
+        <PortfolioItem source="/beata3.png" href="https://beatalakota.pl/" title="Beata Łakota" text="Strona internetowa z wykorzystaniem TypeJS." />
         <PortfolioItem source="/ozonowanie.png" href="https://eos-ozonowanie.pl/" title="EOS - ozonowanie | Kraków" text="Strona Główna z formularzem kontaktowym PHPMAILER." />
         <PortfolioItem source="/ptaki.png" href="https://nshkipe.com.pl/" title="NSHKiPE - Nowy Sącz" text="Strona wraz z panelem admina napisanym w PHP + mySQL." />
-        <PortfolioItem source="/ericar.png" href="https://github.com/Gho2st" title="Ericar - Github." text="Projekt w którym wykorzystany był php, baza danych. Znajduje sie na github." />
+        <PortfolioItem source="/ericar.png" href="https://github.com/Gho2st" title="Ericar" text="Projekt w którym wykorzystany był php, baza danych. Znajduje sie na github." />
         <PortfolioItem source="/dominik.png" href="https://dominikjojczyk.pl/" title="Dominik Jojczyk - portfolio" text="Mój projekt portfolio napisany całkowicie od zera w HTMl, CSS, JS." />
         <PortfolioItem source="/olmart.png" href="https://olmart-ns.pl/" title="Olmart - Zakład Zduński" text="Strona z wykorzystaniem Bootstrapa i JQuery." />
         <PortfolioItem source="/dna.png" href="https://github.com/Gho2st" title="Analiza sekwencji DNA" text="Aplikacja stworzona jako projekt bioinformatyczny." />
