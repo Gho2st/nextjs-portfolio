2cfb72381a98023cd82bda6f44fcfb2434d1ce1e
Dominik <dominik.jojczyk@gmail.com>
2024-03-01 23:15:07 +0100

    delete old project

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -21,11 +21,10 @@ export default function Portfolio() {
         <PortfolioItem source="/beata3.png" href="https://beatalakota.pl/" title="Beata Łakota" text="Strona internetowa z wykorzystaniem TypeJS." />
         <PortfolioItem source="/ozonowanie.png" href="https://eos-ozonowanie.pl/" title="EOS - ozonowanie | Kraków" text="Strona Główna z formularzem kontaktowym PHPMAILER." />
         <PortfolioItem source="/ptaki.png" href="https://nshkipe.com.pl/" title="NSHKiPE - Nowy Sącz" text="Strona wraz z panelem admina napisanym w PHP + mySQL." />
-        <PortfolioItem source="/ericar.png" href="https://github.com/Gho2st" title="Ericar" text="Projekt w którym wykorzystany był php, baza danych. Znajduje sie na github." />
         <PortfolioItem source="/dominik.png" href="https://dominikjojczyk.pl/" title="Dominik Jojczyk - portfolio" text="Mój projekt portfolio napisany całkowicie od zera w HTMl, CSS, JS." />
         <PortfolioItem source="/olmart.png" href="https://olmart-ns.pl/" title="Olmart - Zakład Zduński" text="Strona z wykorzystaniem Bootstrapa i JQuery." />
         <PortfolioItem source="/dna.png" href="https://github.com/Gho2st" title="Analiza sekwencji DNA" text="Aplikacja stworzona jako projekt bioinformatyczny." />
-        <PortfolioItem source="/telmech.png" href="https://telmech.com" title="Telmech - obróbka metali" text="Bootstrap, js, sztuczna inteligencja" />
+        <PortfolioItem source="/telmech.png" href="https://telmech.com" title="Telmech - obróbka metali" text="PHP, zdjęcia generowane przez AI." />
         <PortfolioItem source="/tomy.png" href="https://tomy-myje-remontuje.pl" title="ToMy - Myje & Remontuje" text="Strona dla firmy zajmującej sie pracami porządkowymi" />
       </div>
     </>
