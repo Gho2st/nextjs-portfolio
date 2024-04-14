3be9ed8a80598220a711d4496e9c460251a336e1
Dominik <dominik.jojczyk@gmail.com>
2024-04-14 22:20:47 +0200

    i ve added a new project to portfolio

--- components/portfolio/Portfolio.js
+++ components/portfolio/Portfolio.js
@@ -1,7 +1,6 @@
 import classes from "./Portfolio.module.css";
 import PortfolioItem from "./PortfolioItem";
 
-
 export default function Portfolio() {
   return (
     <>
@@ -17,15 +16,66 @@ export default function Portfolio() {
         </div> */}
       </div>
       <div className={classes.itemContainer}>
-        <PortfolioItem source="/optyk.png" href="https://optyk-andrzej.pl/" title="Optyk - Andrzej Świerczek 1980" text="Strona internetowa wykonana w całości z uzyciem Bootstrapa." />
-        <PortfolioItem source="/beata3.png" href="https://beatalakota.pl/" title="Beata Łakota" text="Strona internetowa z wykorzystaniem TypeJS." />
-        <PortfolioItem source="/ozonowanie.png" href="https://eos-ozonowanie.pl/" title="EOS - ozonowanie | Kraków" text="Strona Główna z formularzem kontaktowym PHPMAILER." />
-        <PortfolioItem source="/ptaki.png" href="https://nshkipe.com.pl/" title="NSHKiPE - Nowy Sącz" text="Strona wraz z panelem admina napisanym w PHP + mySQL." />
-        <PortfolioItem source="/dominik.png" href="https://dominikjojczyk.pl/" title="Dominik Jojczyk - portfolio" text="Mój projekt portfolio napisany całkowicie od zera w HTMl, CSS, JS." />
-        <PortfolioItem source="/olmart.png" href="https://olmart-ns.pl/" title="Olmart - Zakład Zduński" text="Strona z wykorzystaniem Bootstrapa i JQuery." />
-        <PortfolioItem source="/dna.png" href="https://github.com/Gho2st" title="Analiza sekwencji DNA" text="Aplikacja stworzona jako projekt bioinformatyczny." />
-        <PortfolioItem source="/telmech.png" href="https://telmech.com" title="Telmech - obróbka metali" text="PHP, zdjęcia generowane przez AI." />
-        <PortfolioItem source="/tomy.png" href="https://tomy-myje-remontuje.pl" title="ToMy - Myje & Remontuje" text="Strona dla firmy zajmującej sie pracami porządkowymi" />
+        <PortfolioItem
+          source="/optyk.png"
+          href="https://optyk-andrzej.pl/"
+          title="Optyk - Andrzej Świerczek 1980"
+          text="Strona internetowa wykonana w całości z uzyciem Bootstrapa."
+        />
+        <PortfolioItem
+          source="/beata3.png"
+          href="https://beatalakota.pl/"
+          title="Beata Łakota"
+          text="Strona internetowa z wykorzystaniem TypeJS."
+        />
+        <PortfolioItem
+          source="/ozonowanie.png"
+          href="https://eos-ozonowanie.pl/"
+          title="EOS - ozonowanie | Kraków"
+          text="Strona Główna z formularzem kontaktowym PHPMAILER."
+        />
+        <PortfolioItem
+          source="/ptaki.png"
+          href="https://nshkipe.com.pl/"
+          title="NSHKiPE - Nowy Sącz"
+          text="Strona wraz z panelem admina napisanym w PHP + mySQL."
+        />
+        <PortfolioItem
+          source="/dominik.png"
+          href="https://dominikjojczyk.pl/"
+          title="Dominik Jojczyk - portfolio"
+          text="Mój projekt portfolio napisany całkowicie od zera w HTMl, CSS, JS."
+        />
+        <PortfolioItem
+          source="/olmart.png"
+          href="https://olmart-ns.pl/"
+          title="Olmart - Zakład Zduński"
+          text="Strona z wykorzystaniem Bootstrapa i JQuery."
+        />
+        <PortfolioItem
+          source="/dna.png"
+          href="https://github.com/Gho2st"
+          title="Analiza sekwencji DNA"
+          text="Aplikacja stworzona jako projekt bioinformatyczny."
+        />
+        <PortfolioItem
+          source="/telmech.png"
+          href="https://telmech.com"
+          title="Telmech - obróbka metali"
+          text="PHP, zdjęcia generowane przez AI."
+        />
+        <PortfolioItem
+          source="/tomy.png"
+          href="https://tomy-myje-remontuje.pl"
+          title="ToMy - Myje & Remontuje"
+          text="Strona dla firmy zajmującej sie pracami porządkowymi."
+        />
+        <PortfolioItem
+          source="/wesolewygibasy.png"
+          href="https://wesolewygibasy.pl"
+          title="Żłobki Wesołe Wygibasy"
+          text="Projekt w NextJS dla krakowskiej firmy."
+        />
       </div>
     </>
   );
